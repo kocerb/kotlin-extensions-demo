56e4d06c192f30ca4eaf308613cffd69827eb32e
burakkocer <burak.kocer@d-teknoloji.com.tr>
2022-01-10 23:09:35 +0300

    apply scope function

--- src/main/kotlin/com/vomoxada/kotlinextensionsdemo/KotlinExtensionsDemoApplication.kt
+++ src/main/kotlin/com/vomoxada/kotlinextensionsdemo/KotlinExtensionsDemoApplication.kt
@@ -34,13 +34,13 @@ class CustomerRepository(
             VALUES ( :email, :phoneNumber, :firstName, :lastName )
             """.trimIndent()
 
-		var executeSpec = r2dbcEntityTemplate.databaseClient.sql(query)
-
-		executeSpec = customer.email?.let { executeSpec.bind("email", customer.email) } ?: executeSpec.bindNull("email", String::class.java)
-		executeSpec = customer.phoneNumber?.let { executeSpec.bind("phoneNumber", customer.phoneNumber) } ?: executeSpec.bindNull("phoneNumber", String::class.java)
-		executeSpec = customer.firstName?.let { executeSpec.bind("firstName", customer.firstName) } ?: executeSpec.bindNull("firstName", String::class.java)
-		executeSpec = customer.lastName?.let { executeSpec.bind("lastName", customer.lastName) } ?: executeSpec.bindNull("lastName", String::class.java)
-
-		executeSpec.then().awaitFirstOrNull()
+		r2dbcEntityTemplate.databaseClient
+			.sql(query)
+			.apply { customer.email?.let { bind("email", customer.email) } ?: bindNull("email", String::class.java) }
+			.apply { customer.phoneNumber?.let { bind("phoneNumber", customer.phoneNumber) } ?: bindNull("phoneNumber", String::class.java) }
+			.apply { customer.firstName?.let { bind("firstName", customer.firstName) } ?: bindNull("firstName", String::class.java) }
+			.apply { customer.lastName?.let { bind("lastName", customer.lastName) } ?: bindNull("lastName", String::class.java) }
+			.then()
+			.awaitFirstOrNull()
 	}
 }
